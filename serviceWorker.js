17fe3972ab77508133084330cdbb63b6d2fdf0e4
“Mrityunjay <mrityunjay38@gmail.com>
2023-01-11 23:00:17 +0530

    Basic chrome extension init

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,2 @@
-chrome.runtime.onInstalled.addListener(async () => {
-  // Here goes everything you want to execute after extension initialization
-  console.log("Extension successfully installed!");
-});
+// Here goes everything you want to execute after extension initialization
+console.log("Extension successfully installed!");
