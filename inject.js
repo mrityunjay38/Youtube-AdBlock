ebab94b3d60c8ebf62efdb97c555c70f580b0caf
Mrityunjay <mrityun.jay@indegene.com>
2023-01-12 19:30:56 +0530

    Monkey patched window.fetch to tap into response

--- inject.js
+++ inject.js
@@ -1,3 +1,27 @@
-((message) => {
-  console.log(message);
-})("Injected script");
+/* 
+# Monkey patched window.fetch
+# Following script will be injected into YouTube content dom at runtime.
+*/
+
+((originalFetch) => {
+  window.fetch = async (...args) => {
+    let response = await originalFetch(...args);
+    console.log("fetch args:  ", args);
+
+    const json = () =>
+      response
+        .clone()
+        .json()
+        .then((data) => data);
+
+    const text = () =>
+      response
+        .clone()
+        .text()
+        .then((data) => data);
+
+    response.json = json;
+    response.text = text;
+    return response;
+  };
+})(window.fetch);
