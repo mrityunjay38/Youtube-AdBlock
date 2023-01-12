d30cab861f629ae5aaa9c4c8f42c4c43633ef712
Mrityunjay <mrityun.jay@indegene.com>
2023-01-12 19:55:10 +0530

    Modify /youtubei/v1/player API call response to remove advertisment json configs

--- inject.js
+++ inject.js
@@ -1,24 +1,40 @@
 /* 
-# Monkey patched window.fetch
-# Following script will be injected into YouTube content dom at runtime.
+Monkey patched window.fetch
+Following script will be injected into YouTube content dom at runtime.
+
+As per YouTube API calls < https://www.youtube.com/youtubei/v1/player/* >,
+seems to be holding all the advertisment related configurations.
+
+Params: ["adPlacements", "playerAds"]
+
+Removing these shall interrupt short video advertisment previews that appear
+before an actual video that user is trying to stream/play.
 */
 
 ((originalFetch) => {
   window.fetch = async (...args) => {
     let response = await originalFetch(...args);
-    console.log("fetch args:  ", args);
+    const stringReplacementMap = {
+      adPlacements: "adPlacementRemoved",
+      playerAds: "playerAdsRemoved",
+    };
 
+    /* Modifying both json() as well text() response methods to be double sure */
     const json = () =>
       response
         .clone()
         .json()
-        .then((data) => data);
+        .then((data) => ({ ...data, adPlacements: [], playerAds: [] }));
 
     const text = () =>
       response
         .clone()
         .text()
-        .then((data) => data);
+        .then((data) =>
+          data.replace(/adPlacements|playerAds/gi, (substring) => {
+            return stringReplacementMap[substring];
+          })
+        );
 
     response.json = json;
     response.text = text;
